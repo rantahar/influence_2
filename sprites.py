00b07d58937083df2f3836a9c0c80e785ae845bc
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2023-01-09 14:00:57 +0200

    Some new assets

--- sprites.py
+++ sprites.py
@@ -30,8 +30,9 @@ def load_tileset(filename, tile_height, tile_width):
 
 road_sprite = load_tileset('assets/roads.png', 34, 32)[0][0]
 
-city_1 = load_tileset('assets/town_1_1.png', 256, 256)[0][0]
-city_2 = load_tileset('assets/town_2_1.png', 256, 256)[0][0]
+city_1 = load_tileset('assets/building_2.png', 256, 256)[0][0]
+city_2 = load_tileset('assets/town_1_3.png', 256, 256)[0][0]
+city_3 = load_tileset('assets/town_2_3.png', 256, 256)[0][0]
 farm = load_tileset('assets/farm_1.png', 256, 256)[0][0]
 woodlodge = load_tileset('assets/woodlodge_1.png', 256, 256)[0][0]
 flag = load_tileset('assets/project_1.png', 256, 256)[0][0]
@@ -50,10 +51,10 @@ piece_sprites = {
     'unknown': flag,
     'project': flag,
     "city1": city_1,
-    "city2": city_1,
-    "city3": city_1,
-    "city4": city_2,
-    "city5": city_2,
+    "city2": city_2,
+    "city3": city_3,
+    "city4": city_3,
+    "city5": city_3,
     "road": road_sprite,
     "woodlodge": woodlodge,
     "farm": farm,
